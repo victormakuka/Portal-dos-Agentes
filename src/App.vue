2234fb5cd3cbe145ba6cb1d5227a8bec7e6d10d8
Victor Makuka <149114463+victormakuka@users.noreply.github.com>
2025-05-24 17:06:14 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+ import Login from './components/Login.vue'
+
+
+ 
+</script>
+
+<template>
+ <Login/>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="flex justify-center items-center h-screen border px-24"> 
+    <div class=" container bg-orange-600 w-96 h-90 sm:h-1/2 sm:w-36   flex items-center justify-center text-white  border  rounded-l-lg text-xl md:w-70">
+       <div class="font-bold ">  </div>
+    </div>
+    <div class="container bg-white-900 md:w-135 h-90  sm:h-1/2  border  rounded-r-lg border-orange-600 flex items-center justify-center flex-col  p-4 m">
+         <div class="flex-col md:">
+ 
+              <input type="number" placeholder="Entre com seu Id" class="border-1 border-orange-600 rounded-sm my-4 w-67 md:my-1 text-sm p-2">
+
+              <input type="text" placeholder="Senha enviada pelo seu Agente" class="border-1 border-orange-600 rounded-sm w-67 md:my-4 text-sm sm: flex p-2">
+
+              <input type="button">
+                    
+         </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
