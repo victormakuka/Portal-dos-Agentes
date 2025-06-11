84d5bd899c0a47875bad2aaf7a507d74b6d48153
EdvaldoDantas <135360521+EdvaldoDantas@users.noreply.github.com>
2025-06-11 01:23:40 +0100

    Atualizações
    Makuka, use a abordagem moderna que é o script setup... o roteamento consoante a role já está funcionando... por hoje é tudo para mim... melhor terminar as telas, estás a ver as horas que estou a dormir né??... é preciso sacrificio

--- Servicos/GetData.js
+++ Servicos/GetData.js
@@ -1,18 +1,18 @@
 import api from '../src/components/Requisições/Urlapi'
 
-export function getData(token){
-    return api.get("api/User/userData", {
-        headers: {
-            Authorization: `Bearer ${token.acessToken}`
-        }
-    }).then((response) => {
+export async function getData(token) {
+    try {
+        const response = await api.get("api/User/userData", {
+            headers: {
+                Authorization: `Bearer ${token.acessToken}`
+            }
+        });
         const user = response.data;
-        // console.log("User data fetched successfully:", user);
-        // sessionStorage.setItem('user', JSON.stringify(user));
         return user;
-    }).catch((error) => {
+    } catch (error) {
         console.error("Error fetching user data:", error);
-        throw error; // Re-throw the error for further handling if needed
-    });
+        throw error;
+    }
 }
+
 
